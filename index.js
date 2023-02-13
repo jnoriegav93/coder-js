26cb68ca37fb0b4efb5ad5d3d26715bd9177bf53
Jaime Noriega <jaime.libra93@gmail.com>
2023-02-13 15:32:52 -0500

    Funcionalidades

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //alert('sape');
+
+document.getElementById('calcularEnvio').addEventListener('click',function(){
+    let input1 = document.getElementById('input1').innerHTML;
+    let input2 = document.getElementById('input2').innerHTML;
+    let input3 = document.getElementById('input3').innerHTML;
+    console.log('123',input1,input2,input3);
+});
