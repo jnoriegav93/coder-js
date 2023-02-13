6c3cdf2f4131dd95db976f38ebcaeb7f05969a87
Jaime Noriega <jaime.libra93@gmail.com>
2023-02-13 16:14:14 -0500

    cambios

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //alert('sape');
 
 document.getElementById('calcularEnvio').addEventListener('click',function(){
-    let input1 = document.getElementById('input1').innerHTML;
-    let input2 = document.getElementById('input2').innerHTML;
-    let input3 = document.getElementById('input3').innerHTML;
+    let input1 = document.getElementById('input1').value;
+    let input2 = document.getElementById('input2').value;
+    let input3 = document.getElementById('input3').value;
     console.log('123',input1,input2,input3);
+
+    document.getElementById('result').innerHTML = input1 + ' '+  input2 + ' '+ input3 ;
 });
