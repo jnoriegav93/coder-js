3f4fe137ab89eaeb0c83a00f5dcb6d96d87989ab
Jaime Noriega <jaime.libra93@gmail.com>
2023-02-22 09:06:19 -0500

    'Prueba_JS'

---
+++ prueba.js
@@ -0,0 +1,13 @@
+let inicio;
+do{
+    inicio = parseInt(prompt('Ingrese una opción'));
+    switch(inicio){
+        case 1:
+            alert('eligió 1');
+            break;
+        case 2:
+            alert('eligió 2');
+            break;
+    }
+
+}while(inicio !== 0);
