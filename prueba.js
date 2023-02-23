782b15ff2f9af8f9d58beded7a172cd1b4966af9
Jaime Noriega <jaime.libra93@gmail.com>
2023-02-23 12:20:27 -0500

    'Cambios_pruebajs_2023_02_23'

--- prueba.js
+++ prueba.js
@@ -1,10 +1,9 @@
 let inicio;
 do{
-    inicio = parseInt(prompt('Bienvenido\n\n'+
-    '1. Evaluación\n\nIngrese una opción, para cerrar digite 0'));
+    inicio = parseInt(prompt('Bienvenido\n'+
+    'Digite una opción\n\n1. Evaluación\n\n0. Salir'));
     switch(inicio){
         case 1:
-            alert('eligió 1');
             evaluacion();
             break;
         case 2:
@@ -22,25 +21,61 @@ function evaluacion(){
         correctas: 0,
         incorrectas: 0
     };
-    usuario.nombres = prompt('Bienvenido a la evaluación, ingrese sus nombres');
+    //Preguntas
     let prueba = [
         {   
             preguntaID: 1,
             bloque: 1,
             nroPregunta: 1,
-            pregunta: '¿ (a == b) es igual a (a === b)?',
-            alternativas : {1: 'Sí', 2: 'No', 3: 'N.A.'},
-            respuesta: 1
+            pregunta: '¿Cuáles de estas son marcas para la inserción del código JavaScript en las páginas HTML?',
+            alternativas : {1: '<javascript_code> y </javascript_code>', 
+                            2: '<script> y </script>', 
+                            3: '<?script> y </script?>'},
+            respuesta: 2
         },{   
             preguntaID: 2,
             bloque: 1,
             nroPregunta: 2,
-            pregunta: '¿ (a % b) es igual a (a / b)?',
-            alternativas : {1: 'Sí', 2: 'No', 3: 'N.A.'},
+            pregunta: 'La llamada al código Javascript debe colocarse en:',
+            alternativas : {1: 'La sección Body de la página', 
+                            2: 'Antes de la etiqueta HTML', 
+                            3: 'Puede colocarse en la sección Head o en Body'},
+            respuesta: 3
+        },{   
+            preguntaID: 3,
+            bloque: 1,
+            nroPregunta: 3,
+            pregunta: '¿Cuál es la instrucción usada para devolver un valor en una función de JavaScript?',
+            alternativas : {1: 'Return', 
+                            2: 'Send', 
+                            3: 'Value'},
+            respuesta: 1
+        },{   
+            preguntaID: 4,
+            bloque: 2,
+            nroPregunta: 4,
+            pregunta: 'Para terminar las instrucciones en Javascript se utiliza:',
+            alternativas : {1: 'Un punto y coma', 
+                            2: 'La sentencia End', 
+                            3: 'Un punto y coma o un salto de línea'},
+            respuesta: 3
+        },{   
+            preguntaID: 5,
+            bloque: 2,
+            nroPregunta: 5,
+            pregunta: '¿Cuál de estas instrucciones está correctamente escrita en Javascript?',
+            alternativas : {1: 'if (a==0) alert (a);', 
+                            2: 'if (a=0) print a;', 
+                            3: 'if (a==0) { print [a] }', 
+                            4: 'if (a==0): print a;'},
             respuesta: 1
         }
     ];
     //Iniciar la prueba
+    usuario.nombres = prompt('Bienvenido a la evaluación, ingrese sus nombres (al menos 3 caracteres):');
+    while(usuario.nombres.length < 3){
+        usuario.nombres = prompt('Bienvenido a la evaluación, ingrese sus nombres (al menos 3 caracteres):');
+    }
     prueba.forEach(function(item){
         let respuesta_, pregunta_;
         // Estructura de las preguntas
@@ -50,7 +85,7 @@ function evaluacion(){
         for (const [nro, alternativa] of Object.entries(item.alternativas)) {
             pregunta_ += nro + ') '+ alternativa + '\n';
         }
-        pregunta_ += '\n\nDigite una opción:';
+        pregunta_ += '\nDigite una opción:';
         // Fin de Estructura de las preguntas
         respuesta_ = parseInt(prompt(pregunta_)) || -1;
         while(respuesta_ < 0){
@@ -63,10 +98,18 @@ function evaluacion(){
         usuario.correctas   += item.respuesta === respuesta_ ? 1 : 0;
         usuario.incorrectas += item.respuesta !== respuesta_ ? 1 : 0;
     });
-    let resultado_final_ = 'Estimado(a)' + usuario.nombres + ':\n\n'+
-    'El resultado de su prueba es el siguiente:\n'+
+    let resultado_final_ = '------------------------------------------------\n'+
+    'Estimado(a) ' + usuario.nombres + ':'+
+    '\nEl resultado de su prueba es de ('+(usuario.correctas*100/(usuario.correctas+usuario.incorrectas))+'%):\n'+
     'Correctas:   ' + usuario.correctas + '\nIncorrectas: '+ usuario.incorrectas;
-
     //Mostrar resultados
-    alert('Resultado final:\n\n'+resultado_final_);
+    alert('Resultado final:\n'+resultado_final_);
+
+
+    
+    let resultado_final_html = '<b>Estimado(a) ' + usuario.nombres + ':</b><br>'+
+    'El resultado de su prueba es de ('+(usuario.correctas*100/(usuario.correctas+usuario.incorrectas))+'%):<br>'+
+    'Correctas:   ' + usuario.correctas + '<br>Incorrectas: '+ usuario.incorrectas;
+
+    document.getElementById('resultado_prueba').innerHTML = resultado_final_html;
 }
