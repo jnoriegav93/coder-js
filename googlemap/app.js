b5bc3ee024ccedc729b368f5548943575ff6255c
Jaime Noriega Vásquez <jaime.libra93@gmail.com>
2024-04-14 00:25:19 -0500

    Update app.js

--- googlemap/app.js
+++ googlemap/app.js
@@ -568,7 +568,7 @@ function initMap() {
         fetch(`https://red-api.onrender.com/${apiUrl.tipo}/${apiUrl.funcion}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
+                throw new Error(`HTTP error! Status: ${response.status}.`);
             }
             return response.json();
         })
