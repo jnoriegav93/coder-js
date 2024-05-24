27fc136a1e8128b45404f301c0142059efaedf82
Jaime Noriega V <jaime.libra93@gmail.com>
2024-05-24 15:47:30 -0500

    Update 3_subir_mongodb.py

--- googlemap/resources/py/3_subir_mongodb.py
+++ googlemap/resources/py/3_subir_mongodb.py
@@ -6,7 +6,6 @@ from bson.errors import InvalidDocument
 usuario = "jnoriegav"
 contraseña = "Mongodb.s4p3"
 host = "jnv-cluster.ruojgp0.mongodb.net"
-puerto = "27017"  # Puerto por defecto 
 nombre_base_de_datos = "jnv-cluster"
 
 def validar_dato(dato):
@@ -22,8 +21,8 @@ def validar_dato(dato):
 
 def cargar_json(nombre_archivo, nombre_coleccion):
     # Cadena de conexión
-    # cadena_conexion = f"mongodb://{usuario}:{contraseña}@{host}:{puerto}/{nombre_base_de_datos}
-    cadena_conexion = "mongodb+srv://jnoriegav:Mongodb.s4p3@jnv-cluster.ruojgp0.mongodb.net/?retryWrites=true&w=majority&appName=jnv-cluster"
+    cadena_conexion = f"mongodb+srv://{usuario}:{contraseña}@{host}/?retryWrites=true&w=majority&appName={nombre_base_de_datos}"
+    # cadena_conexion = "mongodb+srv://jnoriegav:Mongodb.s4p3@jnv-cluster.ruojgp0.mongodb.net/?retryWrites=true&w=majority&appName=jnv-cluster"
 
     with open(nombre_archivo, 'r') as file:
         datos = json.load(file)
@@ -32,6 +31,7 @@ def cargar_json(nombre_archivo, nombre_coleccion):
     client = MongoClient(cadena_conexion)
     if client.server_info():
         print(f"Conexión exitosa a {cadena_conexion}")
+        cant_guardados = 0
         db = client['test']  # Reemplaza 'nombre_base_de_datos' con el nombre real de tu base de datos
         
         # Verifica si la colección existe antes de intentar crearla
@@ -43,25 +43,28 @@ def cargar_json(nombre_archivo, nombre_coleccion):
 
         # Parsear el JSON
         datax = json.loads(json.dumps(datos))
-        features = datax['features']
+        # features = datax['features']
         # print(features)
         # Filtra los datos válidos y los inserta en la base de datos
         # datos_validos = [dato for dato in datos if validar_dato(dato)]
         # datos_validos = [dato for dato in features if validar_dato(dato)]
-        datos_validos = [dato for dato in features ]
+        datos_validos = [dato for dato in datax ]
         # print(datos)
         for dato in datos_validos:
             try :
                 db[nombre_coleccion].insert_one(dato)
+                cant_guardados += 1
+                if cant_guardados % 200 == 0:
+                    print(f"Van {cant_guardados} de {len(datos_validos)} registros guardados.")
             except Exception as e:
                 print(f"Error al insertar documento: {e}")
         
-        print(f"Datos guardados en MongoDB con éxito desde el archivo {nombre_archivo}.")
+        print(f"Cantidad de registros guardados en MongoDB desde el archivo '{nombre_archivo}': {cant_guardados}.")
         return True
     else:
         print(f"No se pudo conectar a {cadena_conexion}.")
         return False
 
 # Llamada a la función
-cargar_json('fibra.json', 'fibras')
-cargar_json('postes.json', 'postes_')
+cargar_json('__fibra_final.json', 'hilos')
+# cargar_json('__postes_final.json', 'postes')
