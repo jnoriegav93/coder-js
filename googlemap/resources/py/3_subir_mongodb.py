596ad1b61f798fa0648ae6ee6a1ada2f35ce3c5b
Jaime Noriega V <jaime.libra93@gmail.com>
2024-05-24 15:08:58 -0500

    Update 3_subir_mongodb.py

--- googlemap/resources/py/3_subir_mongodb.py
+++ googlemap/resources/py/3_subir_mongodb.py
@@ -1,5 +1,6 @@
 import json
 from pymongo import MongoClient
+from bson.errors import InvalidDocument
 
 # Variables para la cadena de conexión
 usuario = "jnoriegav"
@@ -8,24 +9,59 @@ host = "jnv-cluster.ruojgp0.mongodb.net"
 puerto = "27017"  # Puerto por defecto 
 nombre_base_de_datos = "jnv-cluster"
 
-
+def validar_dato(dato):
+    try:
+        # Intenta convertir el dato a un diccionario de Python
+        print(dato)
+        _ = json.loads(json.dumps(dato))
+        return True
+    except ValueError:
+        # Imprime un mensaje de advertencia y retorna False
+        print(f"Advertencia: El dato '{dato}' no es un diccionario válido.")
+        return False
 
 def cargar_json(nombre_archivo, nombre_coleccion):
     # Cadena de conexión
-    cadena_conexion = f"mongodb://{usuario}:{contraseña}@{host}:{puerto}/{nombre_base_de_datos}"
+    # cadena_conexion = f"mongodb://{usuario}:{contraseña}@{host}:{puerto}/{nombre_base_de_datos}
+    cadena_conexion = "mongodb+srv://jnoriegav:Mongodb.s4p3@jnv-cluster.ruojgp0.mongodb.net/?retryWrites=true&w=majority&appName=jnv-cluster"
 
     with open(nombre_archivo, 'r') as file:
         datos = json.load(file)
 
+    # Intenta conectar a MongoDB
     client = MongoClient(cadena_conexion)
-    db = client[nombre_base_de_datos]
-    collection = db[nombre_coleccion]
+    if client.server_info():
+        print(f"Conexión exitosa a {cadena_conexion}")
+        db = client['test']  # Reemplaza 'nombre_base_de_datos' con el nombre real de tu base de datos
+        
+        # Verifica si la colección existe antes de intentar crearla
+        if nombre_coleccion not in db.list_collection_names():
+            print(f"Colección {nombre_coleccion} creada.")
+            db.create_collection(nombre_coleccion)
+        else:
+            print(f"Colección {nombre_coleccion} ya existe.")
 
-    # Insertar los datos en MongoDB
-    for dato in datos:
-        collection.insert_one(dato)
+        # Parsear el JSON
+        datax = json.loads(json.dumps(datos))
+        features = datax['features']
+        # print(features)
+        # Filtra los datos válidos y los inserta en la base de datos
+        # datos_validos = [dato for dato in datos if validar_dato(dato)]
+        # datos_validos = [dato for dato in features if validar_dato(dato)]
+        datos_validos = [dato for dato in features ]
+        # print(datos)
+        for dato in datos_validos:
+            try :
+                db[nombre_coleccion].insert_one(dato)
+            except Exception as e:
+                print(f"Error al insertar documento: {e}")
+        
+        print(f"Datos guardados en MongoDB con éxito desde el archivo {nombre_archivo}.")
+        return True
+    else:
+        print(f"No se pudo conectar a {cadena_conexion}.")
+        return False
 
-    print(f"Datos guardados en MongoDB con éxito desde el archivo {nombre_archivo}.")
-
-cargar_json('fibra.json','fibra_2')
-cargar_json('postes.json','postes_2')
+# Llamada a la función
+cargar_json('fibra.json', 'fibras')
+cargar_json('postes.json', 'postes_')
